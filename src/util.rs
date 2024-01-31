18ce6e66a91c8e1fe77800fea279ca272c4a2b11
Idra <percy.baker.0@gmail.com>
2024-01-31 17:43:11 +0000

    Fix angle normalization

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,10 @@
 use std::f64::consts::PI;
 
 /// Normalizes between 0 and 2pi
-pub fn normalize_angle(theta: f64) -> f64 {
-    theta.signum() * (theta % (2.0 * PI))
+/// https://stackoverflow.com/questions/31210357/is-there-a-modulus-not-remainder-function-operation
+pub fn normalize_angle(mut theta: f64) -> f64 {
+    theta = theta % (2.0 * PI);
+    (theta + 2.0 * PI) % (2.0 * PI)
 }
 
 #[cfg(test)]
@@ -13,8 +15,10 @@ mod test {
 
     #[test]
     fn test_normalize_angle() {
+        println!("{}", normalize_angle(2.01 * PI));
         assert!((normalize_angle(2.01 * PI) - 0.01 * PI).abs() < 1.0e-5);
         assert!((normalize_angle(3.789 * PI) - 1.789 * PI).abs() < 1.0e-5);
-        assert!((normalize_angle(-1.345 * PI) - 1.345 * PI).abs() < 1.0e-5);
+        assert!((normalize_angle(-1.345 * PI) - 0.655 * PI).abs() < 1.0e-5);
+        assert!((normalize_angle(-6.0 * PI + 0.2) - 0.2).abs() < 1.0e-5);
     }
 }
