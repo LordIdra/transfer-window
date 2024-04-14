ffa4039dc65cb104861c6dce28bfc13cfeefd05c
LordIdra <percy.baker.0@gmail.com>
2024-04-14 23:37:25 +0100

    Prevent creating burns before existing burn

--- view/src/game/overlay/selected_point.rs
+++ view/src/game/overlay/selected_point.rs
@@ -3,42 +3,49 @@ use transfer_window_model::{components::trajectory_component::orbit::Orbit, stor
 
 use crate::{events::Event, game::{underlay::selected::{burn::BurnState, segment_point::SegmentPointState, Selected}, util::format_time, Scene}};
 
-fn draw_next(time: f64, period: f64, segment: &Orbit, ui: &mut Ui, view: &mut Scene, entity: Entity, state: SegmentPointState) {
+fn draw_next(time: f64, period: f64, orbit: &Orbit, ui: &mut Ui, view: &mut Scene, entity: Entity, state: SegmentPointState) {
     let time = time - period;
     let button = Button::new("Previous orbit");
-    let enabled = time > segment.get_current_point().get_time();
+    let enabled = time > orbit.get_current_point().get_time();
     if ui.add_enabled(enabled, button).clicked() {
         view.selected = Selected::Point { entity, time, state };
     }
 }
 
-fn draw_previous(time: f64, period: f64, segment: &Orbit, ui: &mut Ui, view: &mut Scene, entity: Entity, state: SegmentPointState) {
+fn draw_previous(time: f64, period: f64, orbit: &Orbit, ui: &mut Ui, view: &mut Scene, entity: Entity, state: SegmentPointState) {
     let time = time + period;
     let button = Button::new("Next orbit");
-    let enabled = time < segment.get_end_point().get_time();
+    let enabled = time < orbit.get_end_point().get_time();
     if ui.add_enabled(enabled, button).clicked() {
         view.selected = Selected::Point { entity, time, state };
     }
 }
 
-fn draw_orbits(time: f64, model: &Model, period: f64, ui: &mut Ui) {
-    let orbits = ((time - model.get_time()) / period) as usize;
+fn draw_orbits(time: f64, period: f64, orbit: &Orbit, ui: &mut Ui) {
+    let orbits = ((time - orbit.get_start_point().get_time()) / period) as usize;
     if orbits != 0 {
         ui.label("Orbits: ".to_string() + orbits.to_string().as_str());
     }
 }
 
 fn draw_vessel(model: &Model, entity: Entity, ui: &mut Ui, events: &mut Vec<Event>, time: f64, view: &mut Scene, state: SegmentPointState) {
-    if ui.button("Create burn").clicked() {
+    let can_create_burn = if let Some(final_burn) = model.get_trajectory_component(entity).get_final_burn() {
+        time > final_burn.get_start_point().get_time()
+    } else {
+        true
+    };
+
+    let create_burn_button = Button::new("Create burn");
+    if ui.add_enabled(can_create_burn, create_burn_button).clicked() {
         events.push(Event::CreateBurn { entity, time });
         view.selected = Selected::Burn { entity, time, state: BurnState::Selected }
     }
 
-    let segment = model.get_trajectory_component(entity).get_first_segment_at_time(time).as_orbit();
-    if let Some(period) = segment.get_period() {
-        draw_next(time, period, segment, ui, view, entity, state.clone());
-        draw_previous(time, period, segment, ui, view, entity, state);
-        draw_orbits(time, model, period, ui);
+    let orbit = model.get_trajectory_component(entity).get_first_segment_at_time(time).as_orbit();
+    if let Some(period) = orbit.get_period() {
+        draw_next(time, period, orbit, ui, view, entity, state.clone());
+        draw_previous(time, period, orbit, ui, view, entity, state);
+        draw_orbits(time, period, orbit, ui);
     }
 }
 
