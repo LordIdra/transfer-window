85e99b38fe2bdcda1101aafe228f2f30cbb3bbee
LordIdra <percy.baker.0@gmail.com>
2024-04-18 18:19:10 +0100

    Increase tessellation quality

--- view/src/game/underlay/segments/orbit.rs
+++ view/src/game/underlay/segments/orbit.rs
@@ -4,7 +4,7 @@ use nalgebra_glm::DVec2;
 use transfer_window_model::components::trajectory_component::orbit::Orbit;
 
 const INITIAL_POINT_COUNT: usize = 50;
-const TESSELLATION_THRESHOLD: f64 = 1.0e-3;
+const TESSELLATION_THRESHOLD: f64 = 1.0e-4;
 const EXTRA_MIN_DISTANCE: f64 = 1.0e-3;
 
 /// Uses triangle heuristic as described in <https://www.kerbalspaceprogram.com/news/dev-diaries-orbit-tessellation>
