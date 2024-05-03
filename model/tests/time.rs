eaccd2a092b469a90d6c1d6defff81f31eeee0b1
LordIdra <percy.baker.0@gmail.com>
2024-05-03 17:35:06 +0100

    Split warp tests into their own thing

--- model/tests/time.rs
+++ model/tests/time.rs
@@ -14,20 +14,3 @@ fn test_time_step() {
     model.update(1.0);
     assert!(model.get_time() == model.get_time_step().get_time_step());
 }
-
-#[test]
-fn test_warp_1() {
-    let mut model = Model::default();
-    model.start_warp(100.0);
-    model.update(10.0);
-    assert!((model.get_time() - 95.0).abs() < 0.1);
-}
-
-#[test]
-fn test_warp_2() {
-    let mut model = Model::default();
-    model.update(15.0);
-    model.start_warp(100.0);
-    model.update(7.0);
-    assert!((model.get_time() - 95.0).abs() < 0.1);
-}
---
+++ model/tests/warp.rs
@@ -0,0 +1,18 @@
+use transfer_window_model::Model;
+
+#[test]
+fn test_warp_1() {
+    let mut model = Model::default();
+    model.start_warp(100.0);
+    model.update(10.0);
+    assert!((model.get_time() - 95.0).abs() < 0.1);
+}
+
+#[test]
+fn test_warp_2() {
+    let mut model = Model::default();
+    model.update(15.0);
+    model.start_warp(100.0);
+    model.update(7.0);
+    assert!((model.get_time() - 95.0).abs() < 0.1);
+}
