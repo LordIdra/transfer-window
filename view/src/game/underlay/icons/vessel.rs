689c7ca450dc89e14640677381e1a820f063e8f2
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2024-05-06 14:11:11 +0100

    Always show selected target

--- view/src/game/underlay/icons/vessel.rs
+++ view/src/game/underlay/icons/vessel.rs
@@ -25,7 +25,7 @@ impl Vessel {
 
 impl Icon for Vessel {
     fn texture(&self, view: &Scene, model: &Model) -> String {
-        if let Selected::Vessel(entity) = view.selected {
+        if let Some(entity) = view.selected.selected_entity() {
             if let Some(target) = model.vessel_component(entity).target() {
                 if target == self.entity {
                     return "spacecraft-target".to_string()
