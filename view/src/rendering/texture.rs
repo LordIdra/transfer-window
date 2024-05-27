78e3b2bb3e29ab2a890af55f7de4831e901ce13d
LordIdra <percy.baker.0@gmail.com>
2024-05-28 00:30:03 +0100

    Enable linear filtering for textures

--- view/src/rendering/texture.rs
+++ view/src/rendering/texture.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use eframe::glow::{self, NEAREST};
+use eframe::glow::{self, LINEAR, LINEAR_MIPMAP_LINEAR, NEAREST};
 use glow::{Context, HasContext, TEXTURE_2D, RGBA, UNSIGNED_BYTE, TEXTURE_MAG_FILTER, TEXTURE_MIN_FILTER};
 
 pub struct Texture {
@@ -14,8 +14,8 @@ impl Texture {
             let texture = gl.create_texture().expect("Failed to create texture");
             gl.bind_texture(TEXTURE_2D, Some(texture));
             gl.tex_image_2d(TEXTURE_2D, 0, RGBA as i32, size.0, size.1, 0, RGBA, UNSIGNED_BYTE, Some(bytes));
-            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MIN_FILTER, NEAREST as i32);
-            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MAG_FILTER, NEAREST as i32);
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR_MIPMAP_LINEAR as i32);
+            gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MAG_FILTER, LINEAR as i32);
             gl.generate_mipmap(TEXTURE_2D);
             Texture { gl, texture }
         }
